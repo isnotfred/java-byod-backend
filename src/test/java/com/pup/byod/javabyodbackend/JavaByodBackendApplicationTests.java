11c5af03b2b58008089e254641ef59f41b6a5cb4
isnotfred <fredorlain5@gmail.com>
2026-05-26 00:09:55 +0800

    initial setup: Spring Boot + PostgreSQL + Railway config

---
+++ src/test/java/com/pup/byod/javabyodbackend/JavaByodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pup.byod.javabyodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaByodBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
