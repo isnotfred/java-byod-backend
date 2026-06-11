8ca5e7d12e0b067197042ab4cc6a422fc3deac95
isnotfred <fredorlain5@gmail.com>
2026-06-11 17:51:01 +0800

    fix: remove nonexistent database type cast for event_type in DeviceLogDAO

--- src/main/java/com/pup/byod/javabyodbackend/dao/DeviceLogDAO.java
+++ src/main/java/com/pup/byod/javabyodbackend/dao/DeviceLogDAO.java
@@ -224,7 +224,7 @@ public class DeviceLogDAO {
                 ) VALUES (
                     :deviceId,
                     :studentId,
-                    :eventType::gate_event_type,
+                    :eventType,
                     :eventTime,
                     :handledBy,
                     :logoutType,
