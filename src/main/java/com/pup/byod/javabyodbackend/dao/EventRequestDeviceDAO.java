406c62defa755b0f468e4d031fccbef829339d31
isnotfred <fredorlain5@gmail.com>
2026-06-13 22:28:00 +0800

    fix: resolve compilation failure due to missing SQLException import in EventRequestDeviceDAO

--- src/main/java/com/pup/byod/javabyodbackend/dao/EventRequestDeviceDAO.java
+++ src/main/java/com/pup/byod/javabyodbackend/dao/EventRequestDeviceDAO.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
